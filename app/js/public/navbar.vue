094d471d22412bc2f65568ea7e96a0f835dc2918
linhanqiu <fromhanq@163.com>
2018-07-24 16:42:15 +0800

    修改 navbar

--- app/js/public/navbar.vue
+++ app/js/public/navbar.vue
@@ -10,25 +10,25 @@
             <li>
                 <router-link :to="{ name: 'home'}">
                     <img src="//img12.360buyimg.com/jrpmobile/jfs/t7690/252/3621724688/1715/757a3ca1/59e9d81bN27304850.png?width=60&amp;height=60" alt="">
-                    <p>赚钱</p>
+                    <p>理财</p>
                 </router-link>
             </li>
             <li>
                 <router-link :to="{ name: 'home'}">
                     <img src="//img12.360buyimg.com/jrpmobile/jfs/t11653/76/201131398/1192/bf1b2852/59e9d7f3N5f691137.png?width=60&amp;height=60" alt="">
-                    <p>借钱</p>
+                    <p>白条</p>
                 </router-link>
             </li>
             <li>
                 <router-link :to="{ name: 'home'}">
                     <img src="//img12.360buyimg.com/jrpmobile/jfs/t19729/201/1027312338/1395/95cecd48/5ab85d0eN01f81011.png?width=56&amp;height=56" alt="">
-                    <p>省钱</p>
+                    <p>众筹</p>
                 </router-link>
             </li>
             <li>
                 <router-link :to="{ name: 'home'}">
                     <img src="//img12.360buyimg.com/jrpmobile/jfs/t10897/141/1894216742/2046/ad0e8dc1/59e9d7feNaa5dd2fd.png?width=60&amp;height=60" alt="">
-                    <p>金融会员</p>
+                    <p>我的</p>
                 </router-link>
             </li>
         </ul>
@@ -52,7 +52,7 @@ export default {
     bottom: 0;
     height: 100px;
     margin:0;
-    z-index: 100;
+    // z-index: 100;
     >h4{
         display: none;
     }
